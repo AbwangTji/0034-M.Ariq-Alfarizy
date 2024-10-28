5ef85dac4bcf53c85a2aee28f2c4d1bfa475bc19
AbwangTji <ariqa5444@gmail.com>
2024-10-28 08:12:30 +0700

    Create index.php

---
+++ pertemuan4-0034/app/view/user/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User List</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="mb-4">User List</h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Picture</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($users as $user): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($user['id']); ?></td>
+                    <td><?php echo htmlspecialchars($user['name']); ?></td>
+                    <td><?php echo htmlspecialchars($user['email']); ?></td>
+                    <td>
+                        <img src="<?php echo $user['picture'] ? 'assets/images/' . htmlspecialchars($user['picture']) : 'assets/images/default.jpg'; ?>" 
+                             alt="Profile" class="img-thumbnail" style="width: 50px; height: 50px; object-fit: cover;">
+                    </td>
+                    <td>
+                        <a href="index.php?action=show&id=<?php echo $user['id']; ?>" 
+                           class="btn btn-info btn-sm">Detail</a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
